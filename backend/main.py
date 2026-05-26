73873e81fc868ac5783d4568b9284cafd51282e6
am-prasad <amprasad18@gmail.com>
2026-05-26 14:16:53 +0530

    temp

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ import numpy as np
 
 app = FastAPI(title="EcoGridAI", version="2.0.0")
 
-app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
+app.add_middleware(CORSMiddleware, allow_origins=["10.81.25.110:5500/frontend","*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
 class PredictionInput(BaseModel):
     fuelflow: float
